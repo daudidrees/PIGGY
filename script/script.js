410f43727fa174aa9917d51a6c672044050fdef9
Daud Idrees <m.daudidrees@gmail.com>
2022-05-14 13:56:14 +0500

    added toggle to team

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const message = document.querySelector('.faq-content');
 const showMessage = document.querySelector('.faq-desc-wrapper');
 const faqBtn = document.querySelector('#faq-btn');
+const personContent = document.querySelector(".person__content");
+const personDesc = document.querySelector('.person__desc')
 
 
 message.addEventListener("click", function() {
@@ -20,5 +22,6 @@ document.querySelector('.nav-links').addEventListener('click', function(e) {
   }
 });
 
-
-
+personContent.addEventListener("click", function() {
+  personDesc.classList.toggle('person-desc-active');
+})
