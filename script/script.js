4535e078b9592551db2e3e435e1930ef410c0712
Daud Idrees <m.daudidrees@gmail.com>
2022-05-14 20:53:20 +0500

    added responsive

--- script/script.js
+++ script/script.js
@@ -1,18 +1,11 @@
-const message = document.querySelector('.faq-content');
-const showMessage = document.querySelector('.faq-desc-wrapper');
-const faqBtn = document.querySelector('#faq-btn');
-const personContent = document.querySelector(".person__content");
-const personDesc = document.querySelector('.person__desc')
-
-
-message.addEventListener("click", function() {
-  showMessage.classList.toggle('show');
-  if(showMessage.classList.contains("show")){
-    faqBtn.textContent = "-"
-  } else {
-    faqBtn.textContent = "+"
-  }
-});
+const message = document.querySelectorAll('.faq-content');
+const showMessage = document.querySelectorAll('.faq-desc-wrapper');
+const faqBtn = document.querySelectorAll('#faq-btn');
+const personContent = document.querySelectorAll(".person__content");
+const personDesc = document.querySelectorAll('.person__desc');
+const hamburger = document.querySelector(".hamburger-icon");
+const mblNav = document.querySelector('.mobile-nav')
+const navCross = document.querySelector('.nav-cross-btn')
 
 document.querySelector('.nav-links').addEventListener('click', function(e) {
   e.preventDefault();
@@ -22,6 +15,49 @@ document.querySelector('.nav-links').addEventListener('click', function(e) {
   }
 });
 
-personContent.addEventListener("click", function() {
-  personDesc.classList.toggle('person-desc-active');
+
+function hideAllFaqs (faqs) {
+  faqs.forEach(faq => {
+    if(!faq.nextElementSibling.classList.toggle('show')) {
+      faq.nextElementSibling.classList.add('show')
+    } else {
+      faq.nextElementSibling.classList.remove('show')
+    }
+  })
+}
+
+message.forEach(faq => {
+  faq.addEventListener('click', function () {
+    hideAllFaqs(message)
+    const active = this.nextElementSibling.classList.toggle('show')
+  })
+})
+
+personContent.forEach(el => {
+  el.addEventListener('click', function() {
+    personDesc.forEach(el => {
+      if(el.classList.contain('hidden')) {
+      }
+    })
+  })
+})
+
+// personContent.forEach(el =>{
+//   el.addEventListener("click", function() {
+//     personDesc.classList.remove('hidden');
+// })
+
+
+// for (let i = 0; i < personContent.length; i++) {
+//   personContent[i].addEventListener("click", function() {
+//     personDesc.classList.remove('hidden');
+//   })
+// }
+
+hamburger.addEventListener('click', function(e) {
+  mblNav.classList.toggle('mobile-nav');
+})
+
+navCross.addEventListener('click', function(e) {
+  mblNav.classList.add('mobile-nav')
 })
