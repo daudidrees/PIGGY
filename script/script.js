99d4d486433bd88403559f3b1a93162f80f0338d
Daud Idrees <m.daudidrees@gmail.com>
2022-05-14 13:08:39 +0500

    made 1920 design

---
+++ script/script.js
@@ -0,0 +1,24 @@
+const message = document.querySelector('.faq-content');
+const showMessage = document.querySelector('.faq-desc-wrapper');
+const faqBtn = document.querySelector('#faq-btn');
+
+
+message.addEventListener("click", function() {
+  showMessage.classList.toggle('show');
+  if(showMessage.classList.contains("show")){
+    faqBtn.textContent = "-"
+  } else {
+    faqBtn.textContent = "+"
+  }
+});
+
+document.querySelector('.nav-links').addEventListener('click', function(e) {
+  e.preventDefault();
+  if(e.target.classList.contains('nav-item')){
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({behavior: "smooth"})
+  }
+});
+
+
+
